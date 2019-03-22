704b6e20515d754e439325e8ebdb0a80f76ff1e9
LVK-96 <43728745+LVK-96@users.noreply.github.com>
2019-03-22 22:45:13 +0200

    Added vscode snap

--- post-install.sh
+++ post-install.sh
@@ -7,32 +7,19 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Install packages
-sudo apt-get install curl build-essential tilix chromium-browser vim zsh cmake libreoffice gnome-tweak-tool gnome-session htop python python3 fonts-powerline autoconf automake libgtk-3-dev papirus-icon-theme neofetch
+sudo apt-get install curl build-essential vim zsh cmake libreoffice gnome-tweak-tool gnome-session htop python python3 fonts-powerline autoconf automake libgtk-3-dev papirus-icon-theme neofetch
 
+# snaps for telegram, spotify and vscode
 sudo snap install telegram-desktop
 sudo snap install spotify
+sudo snap install vscode --classic
 
 # Install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
 
-# solArc theme
-git clone https://github.com/apheleia/solarc-theme --depth 1 && cd solarc-theme
-./autogen.sh --prefix=/usr
-sudo make install
-
-# Load gnome config
-dconf load /org/gnome/ < gnome.txt
-
 # Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
-
-# Replace default .zshrc with my config
-sudo cat .zshrc > ~/.zshrc
-source ~/.zshrc
-
-# zsh syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Make zsh default shell
 chsh -s $(which zsh)
