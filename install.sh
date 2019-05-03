3a828e58e5de719a073640980d9b01f201f064da
Leo Kivikunnas <kivikunn@ubuntu-1804.novalocal>
2019-05-03 07:23:47 +0000

    added tmux to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install zsh python3 git build-essential scons curl
+sudo apt-get install zsh python3 git build-essential scons curl tmux
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
