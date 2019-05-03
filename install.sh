9d3fe01eccd18e547bf029ef2c1fd09cbf504794
Leo Kivikunnas <kivikunn@ubuntu-1804.novalocal>
2019-05-03 07:16:22 +0000

    added script to install some basic stuff

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install zsh python3 git build-essential scons curl
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
