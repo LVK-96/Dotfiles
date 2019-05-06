3d1938a7f2922fcba88757f104f3a431048359a3
LVK-96 <43728745+LVK-96@users.noreply.github.com>
2019-05-06 13:41:17 +0300

    Update install_at_work.sh

--- install_at_work.sh
+++ install_at_work.sh
@@ -1,6 +1,22 @@
+# Change to home directory
+cd ~
+
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install vim zsh protoc zlib python3 python3-dev git build-essential scons curl tmux
+# Install programs
+sudo apt-get install build-essential autoconf automake libtool curl unzip python3 python3-dev scons git vim zsh tmux
 
+# Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install protobuff https://github.com/protocolbuffers/protobuf
+git clone https://github.com/protocolbuffers/protobuf.git
+cd protobuf
+git submodule update --init --recursive
+./autogen.sh
+./configure
+make
+make check
+sudo make install
+sudo ldconfig # refresh shared library cache.
