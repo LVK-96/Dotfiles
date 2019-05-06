57dcb5e14ebd0ca663240894b5af6004b66bc729
LVK-96 <43728745+LVK-96@users.noreply.github.com>
2019-05-06 14:05:57 +0300

    Update install_at_work.sh

--- install_at_work.sh
+++ install_at_work.sh
@@ -1,7 +1,6 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-# Install programs
 sudo apt-get install build-essential autoconf automake libtool curl unzip python3 python-dev python3-dev scons git vim zsh tmux libprotobuf-dev python-protobuf protobuf-compiler libgoogle-perftools-dev zlib1g-dev libpng-dev libboost-all-dev 
 
 # Install oh my zsh
