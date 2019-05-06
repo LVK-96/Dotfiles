27237c7b698d3e080fca6513f959d73a9b516861
LVK-96 <43728745+LVK-96@users.noreply.github.com>
2019-05-06 13:29:47 +0300

    Update install_at_work.sh

--- install_at_work.sh
+++ install_at_work.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install vim zsh python3 python3-dev git build-essential scons curl tmux
+sudo apt-get install vim zsh protoc zlib python3 python3-dev git build-essential scons curl tmux
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
