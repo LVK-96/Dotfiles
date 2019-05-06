69479690774d8fe4fd6f415ad0cb0274815e2d05
LVK-96 <43728745+LVK-96@users.noreply.github.com>
2019-05-06 13:49:57 +0300

    Update install_at_work.sh

--- install_at_work.sh
+++ install_at_work.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Install programs
-sudo apt-get install build-essential autoconf automake libtool curl unzip python3 python3-dev scons git vim zsh tmux libprotobuf-dev python-protobuf protobuf-compiler libgoogle-perftools-dev
+sudo apt-get install build-essential autoconf automake libtool curl unzip python3 python3-dev scons git vim zsh tmux libprotobuf-dev python-protobuf protobuf-compiler libgoogle-perftools-dev zlib1g-dev libpng-dev 
 
 # Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
