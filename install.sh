095043367df23a6adcf928069a84c85616d8437e
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-06-29 09:14:27 +0300

    Added install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install git vim zsh tmux curl lxterminal net-tools \
+    -- build-essential snapd i3 nitrogen pcmanfm 
+
+sudo snap install spotify
+sudo snap install code --classic
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+# NordVPN
+wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
+sudo dpkg -i nordvpn-release_1.0.0_all.deb
+sudo apt-get update 
+sudo apt-get install nordvpn
+
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
