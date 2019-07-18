b428614348567db225e584bbb6b412c6aa1c64ed
Leo Kivikunnas <43728745+LVK-96@users.noreply.github.com>
2019-07-18 08:41:27 +0300

    Update install_stuff.sh

--- install_stuff.sh
+++ install_stuff.sh
@@ -4,7 +4,7 @@ sudo apt-get install wget curl apt-transport-https ca-certificates gnupg-agent \
 	software-properties-common
 
 # LLVM
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-7 main" 
 
 # Docker
